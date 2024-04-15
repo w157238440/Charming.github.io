a61c7e20e71fc192017bfa9f80fe1de09728f739
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-16 02:22:56 +0800

    Delete js directory

--- js/about.js
+++
@@ -1,8 +0,0 @@
-// js/about.js
-document.addEventListener("DOMContentLoaded", function() {
-    const aboutImg = document.querySelector('.about-img img');
-
-    aboutImg.addEventListener('click', function() {
-        alert('点击了我的照片！');
-    });
-});
--- js/contact.js
+++
@@ -1,9 +0,0 @@
-// js/contact.js
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById('contact-form');
-
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        alert('消息已发送！');
-    });
-});
