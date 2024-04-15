d645599b2d45fbaf535cf47f324bed9028cf0fab
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-16 00:05:38 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,10 @@
 // js/index.js
 $(document).ready(function() {
-    // Smooth scroll to section
+    // Direct scroll to section
     $('nav ul li a').click(function(e) {
         e.preventDefault();
         const sectionId = $(this).attr('href');
-        console.log('Clicked link:', sectionId); // 输出点击的链接
-        const $targetSection = $(sectionId);
-        console.log('Target section:', $targetSection); // 输出目标部分元素
-        console.log('Target section content:', $targetSection.html()); // 输出目标部分内容
-        const targetOffsetTop = $targetSection.offset().top;
-        console.log('Target offset top:', targetOffsetTop); // 输出目标部分偏移量
-        $('html, body').animate({
-            scrollTop: targetOffsetTop
-        }, 1000, function() {
-            console.log('Scroll animation completed.'); // 输出滚动动画完成信息
-        });
+        const targetOffsetTop = $(sectionId).offset().top;
+        $('html, body').scrollTop(targetOffsetTop);
     });
 });
