b840af2404d856dafcf17012ebf1eb31355a95a5
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-15 23:37:54 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,20 +8,4 @@ $(document).ready(function() {
             scrollTop: $(sectionId).offset().top
         }, 1000);
     });
-
-    // Add dynamic background effect
-    let colors = ['#007bff', '#ff00ff', '#00ff00', '#ff0000'];
-    let currentIndex = 0;
-
-    function changeBackground() {
-        $('.hero-bg').animate({opacity: 0}, function() {
-            $(this).css('background-color', colors[currentIndex]);
-            $(this).animate({opacity: 1});
-        });
-
-        currentIndex = (currentIndex + 1) % colors.length;
-    }
-
-    // Decreased background change interval to 5000ms (5 seconds)
-    setInterval(changeBackground, 20000);
 });
