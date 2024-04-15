c16244a73ca4b0a05f299e587f11e26f787b57f6
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-15 23:04:17 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,19 @@
 // js/index.js
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
     // Smooth scroll to section
-    const scrollToSection = (sectionId) => {
-        const section = document.getElementById(sectionId);
-        section.scrollIntoView({ behavior: 'smooth' });
-    };
-
-    // Add event listeners to navigation links
-    document.querySelectorAll('nav ul li a').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const sectionId = this.getAttribute('href').slice(1);
-            scrollToSection(sectionId);
-        });
+    $('nav ul li a').click(function(e) {
+        e.preventDefault();
+        const sectionId = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(sectionId).offset().top
+        }, 1000);
     });
 
     // Add event listener to "了解更多" button
-    document.querySelector('.btn').addEventListener('click', function() {
-        scrollToSection('about');
+    $('.btn').click(function() {
+        const sectionId = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(sectionId).offset().top
+        }, 1000);
     });
 });
