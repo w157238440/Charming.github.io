90472e68da4f6fd5f6b4d4cbfdb2064f62eb7d27
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-15 23:59:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     $('nav ul li a').click(function(e) {
         e.preventDefault();
         const sectionId = $(this).attr('href');
+        console.log('Clicked link:', sectionId); // 输出点击的链接
+        console.log('Target section:', $(sectionId)); // 输出目标部分元素
         $('html, body').animate({
             scrollTop: $(sectionId).offset().top
         }, 1000);
