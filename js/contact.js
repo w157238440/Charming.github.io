ea8292e5895f82045d65d789f5735005fce6d277
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-15 22:31:44 +0800

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,5 @@
 // js/contact.js
 document.addEventListener("DOMContentLoaded", function() {
-    // 在联系页面的 JavaScript 中可以添加表单验证等功能
     const form = document.getElementById('contact-form');
 
     form.addEventListener('submit', function(event) {
