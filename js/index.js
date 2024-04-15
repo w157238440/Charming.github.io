fd5ff5056b8a842acc021f1a06e39e81f455d6bd
妩媚贵妇 <87791714+w157238440@users.noreply.github.com>
2024-04-15 22:30:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // js/index.js
 document.addEventListener("DOMContentLoaded", function() {
-    // 在主页的 JavaScript 中可以添加一些特效和动画
     const hero = document.querySelector('.hero');
     const btn = document.querySelector('.btn');
 
